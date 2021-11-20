24c5f7df97b9051dcab1e6d74b948d7b056a2585
rivkastrilitz <rivk1471@gmail.com>
2021-11-20 16:26:12 +0200

    connected to firebase and open 2 activityes

---
+++ app/src/main/java/com/example/israel_road/Angel_login.java
@@ -0,0 +1,14 @@
+package com.example.israel_road;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Angel_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_angel_login);
+    }
+}
