ab35c8377d584f973a1f47f96c7eef3da8050108
rivkastrilitz <rivk1471@gmail.com>
2021-11-21 16:40:32 +0200

    add botoons

--- app/src/main/java/com/example/israel_road/MainActivity.java
+++ app/src/main/java/com/example/israel_road/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.israel_road;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Angel_login_click(View view) {
+    }
+
+    public void Traveler_login_click(View view) {
+    }
+
+    public void sign_in_click(View view) {
     }
 }
